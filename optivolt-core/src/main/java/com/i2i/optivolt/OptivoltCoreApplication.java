bc37b9af600073ea0d60b06d9f9bc628e483016e
Muhammet Yusuf Durmaz <yusufdrmz132@gmail.com>
2026-07-21 16:23:47 +0300

    feat: initial spring boot project structure setup

---
+++ optivolt-core/src/main/java/com/i2i/optivolt/OptivoltCoreApplication.java
@@ -0,0 +1,13 @@
+package com.i2i.optivolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptivoltCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptivoltCoreApplication.class, args);
+	}
+
+}
