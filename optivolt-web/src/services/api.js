6df444844914c4c59ddbe393fcf668c33606dd36
dogaes <dogaeski@gmail.com>
2026-07-27 00:43:43 +0300

    Point frontend at Cloudflare tunnel, allow Vercel origins in CORS

--- optivolt-web/src/services/api.js
+++ optivolt-web/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://privileges-polymer-genius-petersburg.trycloudflare.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
